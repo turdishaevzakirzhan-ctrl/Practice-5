50d5454be36a6e38974e10fc25fa83a3b99a9cc9
Турдишаев Закиржан <turdisaevzakirzan@MacBook-Air-Turdisaev.local>
2026-03-01 21:44:43 +0500

    Update

--- regex.py
+++ regex.py
@@ -78,3 +78,57 @@ txt = "The rain in Spain"
 x = re.search(r"\bS\w+", txt)
 print(x.group())
 
+import re
+
+text = "abbb a ab aaaa"
+matches = re.findall(r'ab*', text)
+print(matches)
+
+import re
+
+text = "ab abb abbb abbbb"
+matches = re.findall(r'ab{2,3}', text)
+print(matches)
+
+import re
+
+text = "hello_world test_string Invalid_Word"
+matches = re.findall(r'[a-z]+_[a-z]+', text)
+print(matches)
+
+import re
+
+text = "Hello World TEST Python"
+matches = re.findall(r'[A-Z][a-z]+', text)
+print(matches)
+
+import re
+
+text = "a123b axxb a---b ab ac"
+matches = re.findall(r'a.*?b', text)
+print(matches)
+
+import re
+
+text = "Hello, world. Python is cool"
+result = re.sub(r'[ ,.]', ':', text)
+print(result)
+
+text = "hello_world_test"
+
+parts = text.split("_")
+camel = parts[0] + "".join(word.capitalize() for word in parts[1:])
+print(camel)
+
+import re
+
+text = "HelloWorldPython"
+result = re.findall(r'[A-Z][^A-Z]*', text)
+print(result)
+
+import re
+
+text = "helloWorldPython"
+snake = re.sub(r'([A-Z])', r'_\1', text).lower()
+print(snake)
+
